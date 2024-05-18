ca49d912e27342e70c96ddfa397b302fab9e311b
unknown <wsms8646@gmail.com>
2024-05-18 10:18:22 +0900

    prompt engineering

--- use_gpt4.py
+++ use_gpt4.py
@@ -2,7 +2,13 @@ from dotenv import load_dotenv
 import openai
 import json
 import os
+import pandas as pd
 
+df=pd.read_csv('./processed_XSS_dataset.csv')
+print(df)
+answer_df=pd.read_csv('./GPT_XSS_Answer.csv')
+df=df.iloc[len(answer_df):]
+print(df)
 load_dotenv()
 print(os.getenv("OPENAI_API_KEY"))
 openai.api_key=os.getenv("OPENAI_API_KEY")
@@ -10,42 +16,50 @@ f = open("./prompt.txt", 'r')
 whole = f.read()
 
 client = openai.OpenAI()
+for idx, data in enumerate(df['Sentence'].values):
+    print(f"=============================={idx}==================================")
+    response=client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "Suppose you are a security expert at XSS detection."
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": whole,
+                    }
+                ]
+            },
+            {
+                "role": "assistant",
+                "content": "Okay, I will reponse as your instruction"
+            },
+            {
+                "role": "user",
+                "content": f"""{data}"""
+            }
+        ],
+        max_tokens=4096
+    )
+    result=response.choices[0].message.content
+    result=result.strip("`""json""\n")
+    print(result)
+    result=json.loads(result)
+    print(f"CoT_Process: {result['CoT_Process']}")
+    print(f"Vuln: {result['Expected_Vulnerabilities']}")
+    print(f"Detection: {result['XSS_Detection']}")
+    pd.DataFrame({
+        'GPT_Input':[data],
+        'GPT_CoT':[result['CoT_Process']],
+        'GPT_Vuln':[result['Expected_Vulnerabilities']],
+        'GPT_Detection':[result['XSS_Detection']]
+    }).to_csv("./GPT_XSS_Answer.csv", mode='a', header=False, index=False)
 
-response=client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {
-            "role": "system",
-            "content": "Suppose you are a security expert at XSS detection."
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": whole,
-                }
-            ]
-        },
-        {
-            "role": "assistant",
-            "content": "Okay, I will reponse as your instruction"
-        },
-        {
-            "role": "user",
-            "content": """<meta name="viewport" content="width=device-width, initial-scale=1.0">"""
-        }
-    ],
-    max_tokens=4096
-)
-result=response.choices[0].message.content
-print(result)
-result=json.loads(response.choices[0].message.content.strip("`""json""\n"))
-print(result)
-print(type(result))
-print(result['CoT_Process'])
-print(result['Expected_Vulnerabilities'])
-print(result['XSS_Detection'])
+    
 # {
 #     CoT_Process: {
 #         Step_1: <your answer about Step 1>,
